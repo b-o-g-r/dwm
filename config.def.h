204af2d3e4b62bc32bee5c37d2e2fa69d425c106
bogr <acorns.plonks.0y@icloud.com>
2023-01-05 10:48:58 +0200

    chrome remote desktop config for work

--- config.def.h
+++ config.def.h
@@ -53,9 +53,7 @@ static const Rule rules[] = {
        monitor */
     {"Firefox", NULL, NULL, 1 << 0, 1, 0, -1},
     {"Google-chrome", NULL, NULL, 1 << 0, 1, 0, 0},
-    {"Google-chrome", NULL, "Laptop-Work", 1 << 0, 1, 0, 1},
-    {"Google-chrome", NULL, "Remote Access - Chrome Remote Desktop", 1 << 0, 1,
-     0, 1},
+    {"Chrome-Remote-Desktop", NULL, NULL, 1 << 0, 1, 1, 1},
     {"VSCodium", NULL, NULL, 1 << 1, 1, 0, 1},
 };
 
