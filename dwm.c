53972320ef58eaff7619e6eadbb2748bdc7177ff
bogr <acorns.plonks.0y@icloud.com>
2023-02-16 19:09:37 +0200

    test

--- dwm.c
+++ dwm.c
@@ -1287,8 +1287,8 @@ void drawbar(Monitor *m) {
   x = 0;
   for (i = 0; i < LENGTH(tags); i++) {
 		/* Do not draw vacant tags */
-		if(!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
-			continue;
+		// if(!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
+		// 	continue;
     w = TEXTW(tags[i]);
     drw_setscheme(
         drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
