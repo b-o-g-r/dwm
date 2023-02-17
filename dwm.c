d487cf58444bf4f16f25ad38c68a3d9817cd81a1
bogr <acorns.plonks.0y@icloud.com>
2023-02-17 10:38:19 +0200

    single monocle window

--- dwm.c
+++ dwm.c
@@ -1398,6 +1398,10 @@ void focus(Client *c) {
     XDeleteProperty(dpy, root, netatom[NetActiveWindow]);
   }
   selmon->sel = c;
+
+  if (selmon->lt[selmon->sellt]->arrange == monocle)
+    arrangemon(selmon);
+
   drawbars();
 }
 
@@ -1715,8 +1719,17 @@ void monocle(Monitor *m) {
       n++;
   // if (n > 0) /* override layout symbol */
   // snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]", n);
-  for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
+  // for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
+  for (c = m->stack; c && (!ISVISIBLE(c) || c->isfloating); c = c->snext);
+  if (c && !c->isfloating) {
+    XMoveWindow(dpy, c->win, m->wx, m->wy);
     resize(c, m->wx + vp, m->wy + vp, m->ww - 2 * c->bw - 2 * vp, m->wh - 2 * c->bw -  2 * vp, 0);
+    c = c->snext;
+  }
+
+  for (; c; c = c->snext)
+    if (!c->isfloating && ISVISIBLE(c))
+      XMoveWindow(dpy, c->win, WIDTH(c) * -2, c->y);
 }
 
 void motionnotify(XEvent *e) {
